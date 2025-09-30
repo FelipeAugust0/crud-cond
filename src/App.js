9c33827674006b42bc3459b74a6366d23a32c463
FelipeAugust0 <lipe.augusto2013@gmail.com>
2025-09-29 21:57:29 -0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+// Importando componentes
+import Login from "./components/Login/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = (username, password) => {
+    // Simulação de autenticação
+    if (username === "admin" && password === "123") {
+      setIsLoggedIn(true);
+    } else {
+      alert("Usuário ou senha inválidos!");
+    }
+  }
+
+  if (!isLoggedIn) {
+    return <Login onLogin={handleLogin} />;
+  }
 }
 
 export default App;
