da97ee3313cdd1cdd49f0aef4d578ecc5178c4ed
FelipeAugust0 <lipe.augusto2013@gmail.com>
2025-10-06 20:46:36 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 
 // Importando componentes
-import Login from "./components/Login/Login";
+import Login from "./components/Login.js";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
