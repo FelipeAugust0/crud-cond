455601eac69bd59cfd4730085f9e23dd5517fc2d
FelipeAugust0 <lipe.augusto2013@gmail.com>
2025-10-06 22:49:35 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -2,23 +2,53 @@ import React, { useState } from "react";
 import "./App.css";
 
 // Importando componentes
-import Login from "./components/Login.js";
+import Login from "./components/Login/Login.js";
+import Menu from "./components/Menu/Menu.js"
+import ContactForm from "./components/ContactForm/ContactForm.js"
+import ContactList from "./components/ContactList/ContactList.js"
+import Welcome from "./components//Welcome/Welcome.js"
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleLogin = (username, password) => {
-    // Simulação de autenticação
-    if (username === "admin" && password === "123") {
+  const [ isLoggedIn, setIsLoggedIn ] = useState(false);
+  const [ activeScreen, setActiveScreen] = useState("Welcome");
+  const [ contacts, setContacts] = useState(
+    {id:1, name: "Felipe Augusto", phone: "16 991785082"},
+    {id:2, name: "Beatriz Oliveira", phone:"16 994324543"}
+  );
+
+  const [contactToEdit, setContactToEdit] = useState(null);
+
+  function handleLogin( userName, password ) {
+    if(userName === "admin" && password === "123") {
       setIsLoggedIn(true);
     } else {
-      alert("Usuário ou senha inválidos!");
+      alert("Usuário ou senha inválida!!!")
     }
-  }
+  };
+
+  const handleSaveContact = (contact) => {}
+  const handleDeleteContact = (id) => {}
+  const startEdit = (contact) => {}
+  const showCreateForm = () => {}
+  const handleNavigate = (screen) => {}
 
   if (!isLoggedIn) {
-    return <Login onLogin={handleLogin} />;
-  }
+    return <Login onLogin={handleLogin}/>
+  };
+
+  return(
+    <div>
+      <Menu onNavigate={handleNavigate} onCreate={showCreateForm} />
+      <main className='content'>
+        {activeScreen === "Welcome" && <Welcome />}
+        {activeScreen === "list" && <ContactList contacts={contacts} onEdit={startEdit} onDelete={handleDeleteContact}/>}
+        {activeScreen === "form" && <ContactForm contactToEdit={contactToEdit} onSave={handleSaveContact} />}
+       
+      </main>
+    </div>
+  )
+
 }
 
 export default App;
---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import './ContactForm.css';
+
+function ContactForm ({oneSave, contactToEdit}){
+    return(
+        <div>
+
+
+        </div>
+    )
+}
+export default ContactForm;
